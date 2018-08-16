f6dc907bbe9ae687d885223ecf86d51afff2cd0e
tanechai <tiuseko0120@gmail.com>
2018-08-16 15:46:18 +0900

    若干の間違えを修正。

--- app/interface.hpp
+++ app/interface.hpp
@@ -3,35 +3,35 @@
 class manual{
 public:
   virtual void operator()=0;
-  virtual interface* next();
+  virtual manual* next();
 }
 
 class move_front:public manual{
 public:
   virtual void operator()();
-  virtual interface* next();
+  virtual manual* next();
 }
 
 class move_back:public manual{
 public:
   virtual void operator()();
-  virtual interface* next();
+  virtual manual* next();
 }
 
 class move_left:public manual{
 public:
   virtual void operator()();
-  virtual interface* next();
+  virtual manual* next();
 }
 
 class move_right:public manual{
 public:
   virtual void operator()();
-  virtual interface* next();
+  virtual manual* next();
 }
 
 class move_stop:public manual{
 public:
   virtual void operator()();
-  virtual interface* next();
+  virtual manual* next();
 }
