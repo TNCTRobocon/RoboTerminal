e2e53f2d973b9ee6c49d61babcb57a134deb9faa
terurin <hamusutaateru@live.jp>
2018-08-21 19:20:49 +0900

    wrote arugument decoder

--- app/main.hpp
+++ app/main.hpp
@@ -1,7 +1,19 @@
 #pragma once
+#include <optional>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
-struct Argument {
+class Argument {
+    std::unordered_set<char> options;
+    std::unordered_map<std::string, std::string> parameters;
+    std::string location;
+
+   public:
     Argument(int argc, char** argv);
+    Argument(const Argument&) = default;
+    virtual ~Argument() = default;
+    bool HasOption(char c) const { return options.find(c) != options.end(); }
+    std::optional<std::string> GetValue(const std::string&) const;
+    std::string ToString() const;
 };
