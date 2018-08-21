902a72aef7612adb1f79486431a26825a8f9e088
terurin <hamusutaateru@live.jp>
2018-08-21 16:45:34 +0900

    mainをきれいにした

--- app/main.cpp
+++ app/main.cpp
@@ -1,45 +1,10 @@
-#include <bits/unique_ptr.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <general/gamepad.hpp>
-#include <general/motor.hpp>
-#include <iostream>
-#include <string>
 #include <vector>
-
 using namespace std;
 
-static motor_sptr motor_front_right{nullptr}, motor_front_left{nullptr},
-    motor_back_right{nullptr}, motor_back_left{nullptr};
-static motor_manager_sptr motor_manager{nullptr};
-void motor_init();
-void axis_handler(const GamePad* obj, AxisNames axis, float value);
-void button_handler(const GamePad* obj, ButtonNames button, bool value);
-int main() {
-  GamePad pad("/dev/input/js0");
-  motor_init();
+int main(int argc ,char** argv) {
+  
+//起動モードの取得
 
-  // Trigger Handle
-  pad.SetButtonChangedEvent(button_handler);
-  pad.SetAxisChangedEvent(axis_handler);
 
-  while (1) {
-  }
+
 }
-
-void motor_init() {
-  //モーターの初期化
-  motor_manager =
-      move(MotorManager::GenerateMotorManeger("/dev/ttyUSB0", 115200));
-  //モーターにアドレスを割り当てるTODO正しい値を代入すること
-  auto afr = motor_manager->CreateMotor(1);
-  auto afl = motor_manager->CreateMotor(2);
-  auto abr = motor_manager->CreateMotor(3);
-  auto abl = motor_manager->CreateMotor(4);
-}
-
-void axis_handler(const GamePad* obj, AxisNames axis, float value) {}
-
-void button_handler(const GamePad* obj, ButtonNames button, bool value) {}
---
+++ app/main.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+struct Argument {
+  Argument(int argc, char** argv);
+};
