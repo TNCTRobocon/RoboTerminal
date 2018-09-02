3b90bd1c37b172036125c8c1bf2b01104b2b0aa2
terurin <hamusutaateru@live.jp>
2018-09-02 22:12:35 +0900

    merge from teru-edition with gui

--- app/app.cpp
+++ app/app.cpp
@@ -35,7 +35,6 @@ static void shared_init(int* argc, char*** argv) {
         report->Warn(ReportGroup::GamePad, "Missing GamePad Location");
     }
     //シリアルポートを初期化する
-<<<<<<< HEAD
     auto serial_location = setting->Read("serial");
     auto band = setting->Read("serial-band").value_or("115200");
     if (serial_location) {
@@ -45,15 +44,6 @@ static void shared_init(int* argc, char*** argv) {
         report->Warn(ReportGroup::GamePad, "Missing GamePad Location");
     }
 }
-=======
-    auto serial_location =setting->Read("serial");
-    auto band=setting->Read("serial-band").value_or("115200");
-    /*if (serial_location){
-        MotorManager::GenerateMotorManeger(serial_location->c_str(),stoi(band));
-    }else{
-        report->Warn(ReportGroup::GamePad, "Missing GamePad Location");
-    }*/
->>>>>>> 35f5910a6f51f6d1d27a738f36e4797bcce5356b
 
 int main(int argc, char** argv) {
     shared_init(&argc, &argv);
